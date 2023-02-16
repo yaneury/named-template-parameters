b79175351c12ab11972610d54a8f009771fc47c7
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-02-15 20:27:57 -0800

    Rough sketch of labels library

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
-#include <string>
 #include <iostream>
+#include <type_traits>
 
-#if __cplusplus < 201103L 
-namespace std{
-template<bool B, typename T>
-struct enable_if {};template<typename T>
-struct enable_if<true, T> { typedef T type; };
-}
-#endif
+template<typename T, typename U>
+struct is_same_template : std::false_type {};
 
-#if __cplusplus < 201103L // Prior to C++11
-template< bool B, class T = void >
-struct enable_if_t : public std::enable_if<B, T> {};
-#elif __cplusplus < 201402L // C++11
-// `std::enablie_if_t` polyfill for C++11
-template< bool B, class T = void >
-using enable_if_t = typename std::enable_if<B,T>::type;
-#else // C++14 and above
-using enable_if_t = std::enable_if_t;
-#endif
+template<template<typename...> typename T, typename... A, typename... B>
+struct is_same_template<T<A...>, T<B...>> : std::true_type {};
+
+template<template<auto...> typename T, auto... A, auto... B>
+struct is_same_template<T<A...>, T<B...>> : std::true_type {};
 
 template <typename Default, typename... Args> struct GetValueT;
 
@@ -29,82 +19,37 @@ struct GetValueT<Default, T2, Args...> {
                                        GetValueT<Default, Args...>::value> {};
 
   template <typename Default2, typename T22>
-  struct impl<Default2, T22,
-              enable_if_t<std::is_same<typename Default2::Id_, typename T22::Id_>::value>>
+  struct impl<Default2, T22, std::enable_if_t<is_same_template<Default2, T22>::value>>
       : std::integral_constant<decltype(Default::value), T22::value> {};
 
   static constexpr const auto value = impl<Default, T2>::value;
 };
 
 template <typename Default>
-struct GetValueT<Default>
-    : std::integral_constant<decltype(Default::value), Default::value> {};
+struct GetValueT<Default>: std::integral_constant<decltype(Default::value), Default::value> {};
 
-// Library 
-
-enum Engine {
-  V4 = 1,
-  V6 = 2
+enum Color {
+  Red,
+  Green,
+  Blue
 };
 
-enum DriveTrain {
-  Two = 4,
-  Four = 8,
-  All = 16
-};
+template <Color C>
+struct BackgroundT : std::integral_constant<Color, C> {};
 
-std::string ToString(Engine engine) {
-  if (engine == Engine::V4)
-    return "V4";
-  return "V6";
-}
-
-std::string ToString(DriveTrain dt) {
-  if (dt == DriveTrain::Two)
-    return "Two";
-  if (dt == DriveTrain::Four)
-    return "Four";
-  return "All";
-}
-
-struct Specs {
-  Engine engine;
-  DriveTrain dt;
-};
-
-struct EngineId {};
-
-template <Engine E>
-struct EngineT : std::integral_constant<Engine, E> {
-  using Id_ = EngineId;
-};
-
-struct DriveTrainId {};
-
-template <DriveTrain DT>
-struct DriveTrainT : std::integral_constant<DriveTrain, DT> {
-  using Id_ = DriveTrainId;
-};
+template <Color C>
+struct ForegroundT : std::integral_constant<Color, C> {};
 
 template <class ...Args>
-class Car {
-  public:
-    Specs GetSpecs() const {
-      return Specs{.engine = E, .dt = DT};
-    }
+struct S {
+  static constexpr Color BG = GetValueT<BackgroundT<Color::Red>, Args...>::value;
+  static constexpr Color FG = GetValueT<ForegroundT<Color::Blue>, Args...>::value;
 
-  private:
-    static constexpr Engine E = GetValueT<EngineT<Engine::V4>, Args...>::value;
-    static constexpr DriveTrain DT = GetValueT<DriveTrainT<DriveTrain::Two>, Args...>::value;
+  static constexpr bool Same = FG == BG;
 };
 
-using V6CarWithAWD = Car<EngineT<Engine::V6>, DriveTrainT<DriveTrain::All>>;
-
 int main(int argc, const char* argv[]) {
-  constexpr auto car = V6CarWithAWD();
-
-  std::cout << "Engine: " << ToString(car.GetSpecs().engine) << std::endl;
-  std::cout << "DriveTrain: " << ToString(car.GetSpecs().dt) << std::endl;
-
+  using T = S<ForegroundT<Color::Red>, BackgroundT<Color::Red>>;
+  std::cout << "Same: " << T::Same << std::endl;
   return 0;
 }
